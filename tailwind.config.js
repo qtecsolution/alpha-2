35baa5fa1c2b7407aadde0c152317b3e1b9784c9
developer-kawsar <kawsar.qtec@gmail.com>
2022-12-11 14:11:37 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    extend: {
+
+      colors: {
+       primary:'#E7F2EE',
+       primaryLight:'#FAFEFE',
+       primaryDeep:'#7DB89E',
+       secondary:'#F6CCC3',
+       border:'#DADADA',
+       dark:'#333333',
+
+      },
+      fontFamily :{
+        roboto :['Roboto', "sans-serif"]
+      }
+    },
+    container: {
+      center: true,
+    },
+  },
+  plugins: [],
+}
