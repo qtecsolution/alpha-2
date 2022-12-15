e50ecf8b4d2fa0693f1cb0c59d70a43ca1fc4050
developer-kawsar <kawsar.qtec@gmail.com>
2022-12-15 12:14:52 +0600

    driverlist page added

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
        primaryLight:'#FAFEFE',
        primaryDeep:'#7DB89E',
        buttonBg:'#CEE4DA',
+       buttonBg2:'#F5F5F5',
        secondary:'#F6CCC3',
        border:'#DADADA',
        dark:'#333333',
