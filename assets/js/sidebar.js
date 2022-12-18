dacf65ffaf9dec9985ad8a2aa29a6cc78f6539d7
developer-kawsar <kawsar.qtec@gmail.com>
2022-12-18 15:53:49 +0600

    distribution added

--- assets/js/sidebar.js
+++ assets/js/sidebar.js
@@ -1,73 +1,19 @@
-
-//     const hamburger=document.getElementById('hamburger');
-// const sidebar=document.getElementById('sidebar');
-// const items=document.querySelectorAll('a .text');
-// const logo=document.getElementById("sidebarlogo");
-// const li=document.querySelectorAll('ul li')
-// hamburger.addEventListener('click',()=>{ 
-
-//     // shrink sidebar 
-//     if (sidebar.classList.contains('w-72')) {
-//         sidebar.classList.remove('w-72');
-//         sidebar.classList.add("w-20");
-//                 for (item of items) {
-//                     item.style.opacity='0'
-//                     item.style.display='none'
-//                     item.style.visibility = "hidden";
-//                     logo.style.display='none'
-//         } 
-
-//         for(item of li ) {
-//     if(item.classList.contains('mr-6')){
-//         item.classList.remove('mr-6')
-//     }
-// }
-
+// SIDEBAR EXPANDE FUNCTION
+const toggleSidebar = () => { 
         
-//     } else {
+    document.body.classList.toggle("open")
+    if(document.body.classList.contains('open')) {
+        // SET BODY  OPEN CLASS IN LOCALSTORAGE
+    localStorage.setItem('dmsclass','open') 
+}else {
+        // SET BODY EMPTY CLASS IN LOCALSTORAG
+    localStorage.setItem('dmsclass','empty') 
 
-//         // expand sidebar 
-//         sidebar.classList.remove('w-20');
-//         sidebar.classList.add('w-72');
-//         for (item of items) {
-//             item.style.opacity='1'
-//             item.style.display='flex'
-            
-//             item.style.visibility = "visible";
-//             logo.style.display='flex'
-//         }
-//         for(item of li ) {
-//     if(item.classList.contains('mr-6')==false){
-//         item.classList.add('mr-6')
-//     }
-// }
-//     }
-// })
+}
+}
 
-
-// // default if full sidebar 
-// if(sidebar.classList.contains('w-72')) {
-//     for (item of items) {
-//              item.style.opacity='1'
-//              item.style.visibility = "visible";
-//              logo.style.display='block'
-//         }
-// } else {
-// //   if half sidebar 
-//     for (item of items) {
-//                 item.style.opacity='0'
-//                 item.style.visibility = "hidden";
-//                 logo.style.display='none'
-                
-//         } 
-
-//         for(item of li ) {
-//     if(item.classList.contains('mr-6')){
-//         item.classList.remove('mr-6')
-//     }
-// }
-// } 
-
+let bodyclass=localStorage.getItem('dmsclass');
+document.body.classList.replace(document.body.classList[0],bodyclass)
 
 function accordion() {
     return {
