a22d47e6a7df43e361309543dba07a7f4f05af39
developer-kawsar <kawsar.qtec@gmail.com>
2022-12-21 11:24:58 +0600

    added custom calender

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -15,7 +15,7 @@ $(function() {
 
 $(function() {
     $('input[name="daterange"]').daterangepicker({
-      opens: 'left'
+      opens: 'center'
     }, function(start, end, label) {
       console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
     });
